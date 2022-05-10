changed the metrics to take into account multiclass ; corrected the auc ; implemented moving average threshold based on optimizing the f1 validation score

custom_utils.py
@@ -83,49 +83,70 @@ num_classes = 14 #+empty
 
 
 class metrics :
-    def __init__(self,num_classes):
+    def __init__(self,num_classes,threshold):
         self.num_classes=num_classes
+        self.threshold=threshold
+
+        self.f1_list=np.zeros((14))
+        self.mvg_avg=0.99
 
 
-    def top1(self,true, pred):
-        true = np.argmax(true, axis=1)
-        # labels=np.unique(true)
-        labels = np.arange(0, self.num_classes)
+    def accuracy(self,true, pred):
+        pred=np.where(pred>self.threshold,1,0)
 
-        return top_k_accuracy_score(true, pred, k=1, labels=labels)
 
-    def top5(self,true, pred):
-        true = np.argmax(true, axis=1)
-        labels = np.arange(0, self.num_classes)
 
-        return top_k_accuracy_score(true, pred, k=5, labels=labels)
+        return np.mean(np.where(pred==true,1,0))
+
 
     def f1(self,true, pred):
-        true = np.argmax(true, axis=1)
-        pred = np.argmax(pred, axis=1)
+        pred=np.where(pred>self.threshold,1,0)
 
         return sklearn.metrics.f1_score(true, pred, average='macro')  # weighted??
 
     def precision(self,true, pred):
-        true = np.argmax(true, axis=1)
-        pred = np.argmax(pred, axis=1)
+        pred = np.where(pred > self.threshold,1,0)
         return sklearn.metrics.precision_score(true, pred, average='macro')
 
     def recall(self,true, pred):
-        true = np.argmax(true, axis=1)
-        pred = np.argmax(pred, axis=1)
+        pred = np.where(pred > self.threshold,1,0)
         return sklearn.metrics.recall_score(true, pred, average='macro')
 
     def auc(self,true,pred):
-        #TODO : verify auc works well
-        return sklearn.metrics.auc(true,pred)
+        #TODO :  implement auc
+        true,pred=true.T,pred.T
+        auc=0
+        n=len(true)
+        tpr_list,fpr_list=[],[]
+        cat=0
+        for t,p in zip(true,pred) : #for each class
+            best_auc=0
+            range_list=np.arange(0,1.01,0.01)
+            for ex,threshold in enumerate(range_list) :
+                p=np.where(p>threshold,1,0)
+                tpr=np.mean(np.where(np.logical_and(t==p,t==1),1,0))
+                tnr=np.mean(np.where(np.logical_and(t==p,t==0),1,0))
+                fnr = np.mean(np.where(np.logical_and(t != p, t == 0), 1, 0))
+                fpr = np.mean(np.where(np.logical_and(t != p, t == 1), 1, 0))
+                fpr_list.append(fpr)
+                tpr_list.append(tpr)
+
+                f1=tpr/(tpr+0.5*(fpr+fnr))
+                if f1>self.f1_list[cat] :
+                    self.threshold[cat]=self.mvg_avg*self.threshold[cat]+(1-self.mvg_avg)*threshold
+                    self.f1_list[cat]=f1
+
+            auc+=np.trapz(tpr_list,fpr_list)
+            cat+=1
+
+        return auc/n
 
     def metrics(self):
         dict={
             "f1" : self.f1,
-            "top1" : self.top1,
-            "top5" : self.top5,
+            "auc" : self.auc,
             "recall" : self.recall,
-            "precision" : self.precision
+            "precision" : self.precision,
+            "accuracy" : self.accuracy
         }
         return dict

train.py
@@ -5,6 +5,7 @@ import wandb
 import os
 import argparse
 import torchvision
+import numpy as np
 #-----local imports---------------------------------------
 from models.FCN import FCN
 from training.training import training
@@ -68,14 +69,18 @@ def init_parser() :
                         type=list,
                         nargs='?',
                         required=False,
-                        help="The batch size to use. If > max_batch_size,gradient accumulation will be used")
+                        help="extra tags to add to the logs")
+    parser.add_argument('--debug',
+                        action=argparse.BooleanOptionalAction,
+                        default=False,
+                        help='do you wish  execute small train set in debug mode')
 
     return parser
 
 def main() :
     parser=init_parser()
     args = parser.parse_args()
-
+    os.environ["DEBUG"] = str(args.debug)
 
     criterion = torch.nn.CrossEntropyLoss()
 
@@ -102,11 +107,18 @@ def main() :
 
 
     from custom_utils import metrics
+
+
     train_dataset = CustomImageDataset(f"data/training",num_classes=14, transform=prepro.preprocessing())
     val_dataset = CustomImageDataset(f"data//validation",num_classes=14, transform=prepro.preprocessing())
 
     #rule of thumb : num_worker = 4 * number of gpu
     #batch_size : maximum possible without crashing
+
+    # samples_weight=
+    # samples_weight=torch.tensor(samples_weight)
+    # sampler = torch.utils.data.sampler.WeightedRandomSampler(samples_weight, len(samples_weight))
+
     training_loader = torch.utils.data.DataLoader(train_dataset, batch_size=max_batch_size, shuffle=True, num_workers=0,pin_memory=True)
     validation_loader = torch.utils.data.DataLoader(val_dataset, batch_size=int(max_batch_size*2), shuffle=False, num_workers=0,pin_memory=True)
     print("The data has now been loaded successfully into memory")
@@ -127,7 +139,7 @@ def main() :
     experiment = Experiment(f"{args.model}",is_wandb=args.wandb,tags=args.tags)
 
     optimizer = torch.optim.AdamW(model.parameters())
-    metric=metrics(num_classes=14)
+    metric=metrics(num_classes=14,threshold=np.zeros((14))+0.5)
     metrics=metric.metrics()
     training(model,optimizer,criterion,training_loader,validation_loader,device,minibatch_accumulate=accumulate,epoch_max=args.epoch,patience=5,experiment=experiment,metrics=metrics)
 

training/dataloaders/cxray_dataloader.py
@@ -26,6 +26,8 @@ class CustomImageDataset(Dataset):
 
 
     def __len__(self):
+        if  bool(os.environ["DEBUG"]) :
+            return 100
         return len(self.files)
 
     def label_transform(self,label_ids): # encode one_hot
@@ -62,7 +64,8 @@ class CustomImageDataset(Dataset):
 
 
 
-        image = cv.imread(img_path)
+        image = cv.imread(img_path,cv.IMREAD_GRAYSCALE)
+        image=np.stack((image,)*3,axis=-1)
         if self.transform:
             image=Image.fromarray(np.uint8(image))
             image = self.transform(image)

training/training.py
@@ -33,7 +33,7 @@ def training_loop(model,loader,optimizer,criterion,device,minibatch_accumulate) 
         outputs = model(inputs)
         loss = criterion(outputs, labels)
 
-        results[1] = torch.cat((results[1], torch.nn.functional.softmax(outputs,dim=1).detach().cpu()),dim=0)
+        results[1] = torch.cat((results[1], torch.sigmoid(outputs).detach().cpu()),dim=0)
 
         loss.backward()
         running_loss+=loss.detach()/batch_size
@@ -75,7 +75,7 @@ def validation_loop(model,loader,criterion,device):
 
         outputs = model(inputs)
         loss = criterion(outputs, labels)
-        results[1] = torch.cat((results[1], torch.nn.functional.softmax(outputs,dim=1).detach().cpu()),dim=0)
+        results[1] = torch.cat((results[1], torch.sigmoid(outputs).detach().cpu()),dim=0)
 
         running_loss+=loss.detach()/batch_size
 
@@ -110,7 +110,9 @@ def training(model,optimizer,criterion,training_loader,validation_loader,device=
             experiment.log_metric("validation_loss", val_loss.tolist(),epoch=epoch)
 
             for key in metrics :
-                experiment.log_metric(key,metrics[key](results[1].numpy(),results[0].numpy()),epoch=epoch)
+                pred=results[1].numpy()
+                true=results[0].numpy()
+                experiment.log_metric(key,metrics[key](true,pred),epoch=epoch)
 
         if val_loss<best_loss :
             best_loss=val_loss