16da176f86a0db802fc3eebb4d2d9f24755ca2ee
jonathan <joe199827@gmail..com>
2022-06-02 00:39:42 -0400

    included roc exception

--- Metrics.py
+++ Metrics.py
@@ -1,19 +1,19 @@
 import numpy as np
 from sklearn import metrics
 from sklearn.metrics._ranking import roc_auc_score
-
+import warnings
 import yaml
 
-with open("data/data.yaml","r") as stream :
-    names=yaml.safe_load(stream)["names"]
+with open("data/data.yaml", "r") as stream:
+    names = yaml.safe_load(stream)["names"]
 
-names+=["No Finding"]
+names += ["No Finding"]
+
+
 class Metrics:
     def __init__(self, num_classes, threshold):
         self.num_classes = num_classes
         self.threshold = threshold
-
-
 
     def accuracy(self, true, pred):
         pred = np.where(pred > self.threshold, 1, 0)
@@ -31,13 +31,17 @@ class Metrics:
         pred = np.where(pred > self.threshold, 1, 0)
         return metrics.recall_score(true, pred, average="macro")
 
-    def computeAUROC(self,true,pred):
-
-        outAUROC = {}
-        classCount=pred.shape[1]
-        for i in range(classCount):
-            outAUROC[names[i]]=roc_auc_score(true[:, i], pred[:, i])
-        outAUROC["mean"]=np.mean(list(outAUROC.values()))
+    def computeAUROC(self, true, pred):
+        try:
+            outAUROC = {}
+            classCount = pred.shape[1]
+            for i in range(classCount):
+                outAUROC[names[i]] = roc_auc_score(true[:, i], pred[:, i])
+            outAUROC["mean"] = np.mean(list(outAUROC.values()))
+        except ValueError as e:
+            warnings.warn(e)
+            for i in names + ["mean"]:
+                outAUROC[i] = 0
         return outAUROC
 
     def metrics(self):
