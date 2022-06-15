cb1c07538eb45d1c3b47897cad0e3e04533de6f9
jonathan <joe199827@gmail.com>
2022-06-15 13:03:34 -0400

    added branch condition on test

--- tests/run_tests.sh
+++ tests/run_tests.sh
@@ -1,4 +1,7 @@
-branch_name="$(git symbolic-ref HEAD 2>/dev/null)"
+branch_name="$(git symbolic-ref HEAD 2>/dev/null)" ||
+branch_name="(unnamed branch)"     # detached HEAD
+
+branch_name=${branch_name##refs/heads/}
 if [["$branch_name" == "main" || "$branch_name" == "master"]] ; then
 
    python -m pytest -v .
