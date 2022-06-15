68d9defc3b7b88962ee6cc131feb2d50251f8f81
jonathan <joe199827@gmail.com>
2022-06-15 12:57:44 -0400

    added branch condition on test

--- tests/run_pylint.sh
+++ tests/run_pylint.sh
@@ -1,6 +1,6 @@
 branch_name="$(git symbolic-ref HEAD 2>/dev/null)"
 if (branch_name="main") || (branch_name="master") then
-   python -m pylint -rn ./CheXpert2
+   python -m pylint  --verbose ./CheXpert2
    exit 0
 fi
 
