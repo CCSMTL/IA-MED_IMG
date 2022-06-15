c1901786a819b00137499615ba598ce08757c42c
jonathan <joe199827@gmail.com>
2022-06-15 15:10:38 -0400

    added randaugment params to config

--- tests/run_black.sh
+++ tests/run_black.sh
@@ -8,13 +8,13 @@ master="master"
 if [ "$branch_name"="$main" ] || [ "$branch_name"="$master" ]; then
    python -m black ./CheXpert2
    status=$?
-  if (status=1) then
-    echo "black exited with code 1 ; as always?"
+  if [ $status -eq 1 ]; then
+    exit 1
+   fi
+   if [ $status -eq 0 ]; then
     exit 0
-  fi
-  if (status=0) then
-    exit 0
-  fi
+   fi
+  echo $status
   exit 2
 fi
 echo "Not on main ; black wont run"
--- tests/run_pylint.sh
+++ tests/run_pylint.sh
@@ -7,9 +7,15 @@ main="main"
 master="master"
 if [ "$branch_name"="$main" ] || [ "$branch_name"="$master" ]; then
    python -m pylint  ./CheXpert2
-   code=$?
-   exit 0
+   status=$?
+   if [ $status -eq 1 ]; then
+    exit 1
+   fi
+   if [ $status -eq 0 ] || [ $status -eq 30 ]; then
+    exit 0
+   fi
+  echo $status
+  exit 2
 fi
-
 echo "Not on main ; pylint wont run"
 exit 0
