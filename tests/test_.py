6873354344dc382ef57c363ca0f1aa4fa0a690e0
Jonathan Beaulieu-Emond <Jonathan.Beaulieu-Emond.ccsmtl@ssss.gouv.qc.ca>
2022-06-26 02:54:24 +0000

    Updated test_.py

--- tests/test_.py
+++ tests/test_.py
@@ -29,7 +29,7 @@ urllib.request.install_opener(opener)
 def test_sampler():
     from CheXpert2.Sampler import Sampler
 
-    sampler = Sampler("data_test")
+    sampler = Sampler("tests/data_test")
     samples = sampler.sampler()  # probably gonna break?
 
 
