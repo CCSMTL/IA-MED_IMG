6fc8db3606f54d9051c6b420e834452ef5827ad5
jonathan <joe199827@gmail.com>
2022-08-17 15:21:23 -0400

    fixed bugs

--- CheXpert2/multi_gpu.py
+++ CheXpert2/multi_gpu.py
@@ -10,7 +10,7 @@ import copy
 import numpy as np
 import torch
 import torch.distributed as dist
-
+from torch.utils.data.sampler import SequentialSampler
 from CheXpert2.Experiment import Experiment
 from CheXpert2.models.CNN import CNN
 from CheXpert2.train import main, initialize_config
@@ -26,6 +26,9 @@ if __name__ == "__main__":
     config, img_dir, experiment, device, prob, sampler, names = initialize_config()
     sampler2 = copy.copy(sampler)
     sampler2.weights = 1 / sampler2.weights
+    sampler2 = torch.utils.data.DistributedSampler(SequentialSampler(sampler2))
+    sampler = torch.utils.data.DistributedSampler(SequentialSampler(sampler))
+    optimizer = torch.optim.AdamW
     rank = dist.get_rank()
     device = rank % torch.cuda.device_count()
     # -----------model initialisation------------------------------
@@ -39,13 +42,13 @@ if __name__ == "__main__":
     experiment2 = Experiment(
         f"{config['model']}", names=names, tags=None, config=config, epoch_max=5, patience=5
     )
-    from torch.utils.data.sampler import SequentialSampler
+
 
     model = torch.nn.SyncBatchNorm.convert_sync_batchnorm(model)
     local_rank = int(os.environ['LOCAL_RANK'])
     model = torch.nn.parallel.DistributedDataParallel(model, device_ids=[local_rank])
-    sampler = torch.utils.data.DistributedSampler(SequentialSampler(sampler2))
-    optimizer = torch.optim.AdamW
+
+
     results = main(config, img_dir, model, experiment2, optimizer, torch.nn.BCEWithLogitsLoss(), device, prob, sampler2,
                    metrics=None, pretrain=True)
 
