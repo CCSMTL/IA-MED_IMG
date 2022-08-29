37409fdff6520d738603f40f7f19565ff630b8e0
J-bytes <60334318+J-bytes@users.noreply.github.com>
2022-08-29 14:04:21 -0400

    fixing train/val split

--- fix_database.py
+++ fix_database.py
@@ -19,31 +19,33 @@ def main():
         results = list(coll.find({}))
         columns = results[0].keys()
         data = pd.DataFrame(results,columns=columns)
-        ids=data.groupby("Patient ID")["_id"].apply(list).tolist()
+        ids_lists=data.groupby("Patient ID")["_id"].apply(list).tolist()
 
-        n=len(ids)
+        n=len(ids_lists)
 
         idx=np.random.permutation(n)
         train = np.zeros((n))
         train[idx[0:int(0.85*n)]]=1
 
 
-        for id,train_label in zip(ids,train):
+        for ids,train_label in zip(ids_lists,train):
             valid_label = 1 - train_label
-            print(train_label,valid_label)
-            coll.update_one(
-                {
-                    "_id": id,
+            for id in ids :
 
-                },
-                {
-                    "$set": {
-                        "Train" : int(train_label),
-                        "Valid" : int(valid_label)
+                print(id,train_label,valid_label)
+                coll.update_one(
+                    {
+                        "_id": id,
+
+                    },
+                    {
+                        "$set": {
+                            "Train" : int(train_label),
+                            "Valid" : int(valid_label)
+                        }
                     }
-                }
 
-            )
+                )
 
 
 if __name__ == "__main__":
