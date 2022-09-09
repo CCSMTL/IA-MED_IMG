9d792d1f75e61539d0a78970050de2ed0a7830d4
J-bytes <60334318+J-bytes@users.noreply.github.com>
2022-09-09 16:28:51 -0400

    working on the scheduler

--- CheXpert2/training/training.py
+++ CheXpert2/training/training.py
@@ -7,7 +7,7 @@ from CheXpert2.custom_utils import set_parameter_requires_grad
 
 
 def training_loop(
-        model, loader, optimizer, criterion, device, scaler, clip_norm, autocast
+        model, loader, optimizer, criterion, device, scaler, clip_norm, autocast,scheduler,epoch
 ):
     """
 
@@ -22,8 +22,8 @@ def training_loop(
     running_loss = 0
 
     model.train()
-    i = 0
-    n = len(loader)
+    iters = len(loader)
+    i = 1
     for inputs, labels in loader:
 
         #send to GPU
@@ -62,7 +62,7 @@ def training_loop(
         else:
             optimizer.step()
         # optimizer.step()
-
+        scheduler.step(epoch + i / iters)
         optimizer.zero_grad(set_to_none=True)
         running_loss += loss.detach()
         # ending loop
@@ -164,7 +164,9 @@ def training(
             device,
             scaler,
             clip_norm,
-            autocast
+            autocast,
+            scheduler,
+            epoch=epoch
         )
         if experiment.rank == 0:
             val_loss, results = validation_loop(
@@ -185,7 +187,7 @@ def training(
             # Finishing the loop
 
         experiment.next_epoch(val_loss, model)
-        scheduler.step()
+        epoch += 1
         # if not dist.is_initialized() and experiment.epoch % 5 == 0:
         #     set_parameter_requires_grad(model, 1 + experiment.epoch // 2)
         if experiment.epoch == experiment.epoch_max:
