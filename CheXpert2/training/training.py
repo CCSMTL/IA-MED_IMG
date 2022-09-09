5ac4de1d0369875eca83ab99a12d4dc34f203eed
J-bytes <60334318+J-bytes@users.noreply.github.com>
2022-09-09 16:18:19 -0400

    had to reduce length of cosine

--- CheXpert2/training/training.py
+++ CheXpert2/training/training.py
@@ -148,7 +148,7 @@ def training(
     criterion = criterion(pos_weight=torch.ones((len(experiment.names),),device=device)*pos_weight)
 
     position = device + 1 if type(device) == int else 1
-    scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer=optimizer, T_0=10,T_mult=2)
+    scheduler = torch.optim.lr_scheduler.CosineAnnealingWarmRestarts(optimizer=optimizer, T_0=3,T_mult=2)
     #scheduler = torch.optim.lr_scheduler.ConstantLR(optimizer, factor=0.1)
     while experiment.keep_training:  # loop over the dataset multiple times
         metrics_results = {}
